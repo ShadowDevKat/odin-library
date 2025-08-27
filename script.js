44f8ef4f4ed3975bf463b85c219bb82d065dc903
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-08-27 06:05:09 +0500

    Created mock layout

---
+++ script.js
@@ -0,0 +1,11 @@
+// book array
+const myLibrary = [];
+
+// the constructor...
+function Book() {
+    // should have the book’s id, title, author, the number of pages, and whether or not you have read the book.
+}
+
+// take params, create a book then store it in the array
+function addBookToLibrary() {
+}
