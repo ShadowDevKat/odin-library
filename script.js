9c36dc61a83ec55d28b0edcfe1ebac39f3f771a9
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-08-27 06:30:26 +0500

    Added base functionality to script.js

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 // book array
 const myLibrary = [];
 
-// the constructor...
-function Book() {
-    // should have the book’s id, title, author, the number of pages, and whether or not you have read the book.
+// constructor
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead
+
+    this.info = function () {
+        console.log(`
+            Book ID: ${this.id}
+            Title:   ${this.title}
+            Author:  ${this.author}
+            Pages:   ${this.pages}
+            Read:    ${this.isRead ? "Yes" : "No"}
+            `);
+    };
 }
 
-// take params, create a book then store it in the array
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+
+    myLibrary.push(newBook);
 }
+
+function logBooks() {
+    for (let book of myLibrary) {
+        book.info();
+    }
+}
+
+addBookToLibrary("The Silent Horizon", "Mira Collins", 324, true);
+addBookToLibrary("Shadows of the Valley", "Ethan Ward", 198, false);
+addBookToLibrary("Journey Beyond the Stars", "Aisha Khan", 412, true);
+
+logBooks();
